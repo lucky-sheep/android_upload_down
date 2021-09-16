f579c522865ee3b2dd459aa87c772b16f7eca242
nan_sen <nan_sen@hunliji.com>
2021-09-17 00:15:02 +0800

    1.0.0

---
+++ hlj-download/src/main/java/com/hunliji/hlj_download/upload/model/TokenInfo.kt
@@ -0,0 +1,12 @@
+package com.hunliji.hlj_download.upload.model
+
+/** Created by yc on 2021/9/16
+ **/
+data class TokenInfo(
+    val imageHostUrl:String?=null,
+    val fileUploadToken:String?=null,
+    val headImgFolder:String?=null,
+    val anchorFolder:String?=null,
+    val familyFolder:String?=null
+
+)
